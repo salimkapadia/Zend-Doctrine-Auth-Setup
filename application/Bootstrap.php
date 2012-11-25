8e78d1bb9c25b3f1f11e1a5cdcaf4220448a3886
Salim Kapadia <salimk786@gmail.com>
2012-11-25 01:00:39 +0000

    added comments to the  ini file to indicate its purpose

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -8,5 +8,13 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $view = $this->getResource('view');
         $view->doctype('XHTML1_STRICT');
     }
+    public function _initAutoloaderNamespaces()
+    {
+        require_once APPLICATION_PATH . '/../library/Doctrine/Common/ClassLoader.php';
+
+        $autoloader = \Zend_Loader_Autoloader::getInstance();
+        $fmmAutoloader = new \Doctrine\Common\ClassLoader('Bisna');
+        $autoloader->pushAutoloader(array($fmmAutoloader, 'loadClass'), 'Bisna');
+    }
 }
 
